66f5c64b3c93163b4c8cf6faeefd9dc36dd07e73
JoeDDev <39575347+JoeDDev@users.noreply.github.com>
2019-08-18 23:32:30 -0600

    Add files via upload

---
+++ main_online.js
@@ -0,0 +1,174 @@
+
+	 	 
+	 $( function() {
+        $( "#draggable1" ).draggable();
+        $( "#droppable" ).droppable({
+          drop: function( event, ui ) {
+            $( this )
+              .addClass( "ui-state-highlight" )
+              .find( "p" )
+                
+                  console.log(this.id);
+            $("#draggable1").css("background-color", "blue");
+            /* $("#draggable").draggable("disable"); */
+          }
+          
+          
+        });
+	  } );
+
+	  $( function() {
+        $( "#draggable2" ).draggable();
+        $( "#droppable" ).droppable({
+          drop: function( event, ui ) {
+            $( this )
+              .addClass( "ui-state-highlight" )
+              .find( "p" )
+                .html( "Dropped!2")
+                  console.log(this.id);
+            $("#draggable2").css("background-color", "blue");
+            /* $("#draggable").draggable("disable"); */
+          }
+          
+          
+        });
+	  } );
+
+	  $( function() {
+        $( "#draggable3" ).draggable();
+        $( "#droppable" ).droppable({
+          drop: function( event, ui ) {
+            $( this )
+              .addClass( "ui-state-highlight" )
+              .find( "p" )
+                .html( "Dropped!3")
+                  console.log(this.id);
+            $("#draggable3").css("background-color", "blue");
+            /* $("#draggable").draggable("disable"); */
+          }
+          
+          
+        });
+	  } );
+
+	  $( function() {
+        $( "#draggable4" ).draggable();
+        $( "#droppable" ).droppable({
+          drop: function( event, ui ) {
+            $( this )
+              .addClass( "ui-state-highlight" )
+              .find( "p" )
+                
+                  console.log(this.id);
+            $("#draggable4").css("background-color", "blue");
+            /* $("#draggable").draggable("disable"); */
+          }
+          
+          
+        });
+	  } );
+
+	  $( function() {
+        $( "#draggable5" ).draggable();
+        $( "#droppable" ).droppable({
+          drop: function( event, ui ) {
+            $( this )
+              .addClass( "ui-state-highlight" )
+              .find( "p" )
+                
+                  console.log(this.id);
+            $("#draggable5").css("background-color", "blue");
+            /* $("#draggable").draggable("disable"); */
+          }
+          
+          
+        });
+	  } );
+
+	  $( function() {
+        $( "#draggable6" ).draggable();
+        $( "#droppable" ).droppable({
+          drop: function( event, ui ) {
+            $( this )
+              .addClass( "ui-state-highlight" )
+              .find( "p" )
+                
+                  console.log(this.id);
+            $("#draggable6").css("background-color", "blue");
+            /* $("#draggable").draggable("disable"); */
+          }
+          
+          
+        });
+	  } );
+
+	  $( function() {
+        $( "#draggable7" ).draggable();
+        $( "#droppable" ).droppable({
+          drop: function( event, ui ) {
+            $( this )
+              .addClass( "ui-state-highlight" )
+              .find( "p" )
+                
+                  console.log(this.id);
+            $("#draggable7").css("background-color", "blue");
+            /* $("#draggable").draggable("disable"); */
+          }
+          
+          
+        });
+	  } );
+
+	  $( function() {
+        $( "#draggable8" ).draggable();
+        $( "#droppable" ).droppable({
+          drop: function( event, ui ) {
+            $( this )
+              .addClass( "ui-state-highlight" )
+              .find( "p" )
+                
+                  console.log(this.id);
+            $("#draggable8").css("background-color", "blue");
+            /* $("#draggable").draggable("disable"); */
+          }
+          
+          
+        });
+	  } );
+
+	  $( function() {
+        $( "#draggable9" ).draggable();
+        $( "#droppable" ).droppable({
+          drop: function( event, ui ) {
+            $( this )
+              .addClass( "ui-state-highlight" )
+              .find( "p" )
+                
+                  console.log(this.id);
+            $("#draggable9").css("background-color", "blue");
+            /* $("#draggable").draggable("disable"); */
+          }
+          
+          
+        });
+	  } );
+
+	  $( function() {
+        $( "#draggable" ).draggable();
+        $( "#droppable" ).droppable({
+          drop: function( event, ui ) {
+            $( this )
+              .addClass( "ui-state-highlight" )
+              .find( "p" )
+                
+                  console.log(this.id);
+            $("#draggable").css("background-color", "blue");
+            /* $("#draggable").draggable("disable"); */
+          }
+          
+          
+        });
+	  } );
+
+	  
+	  
